3a8113b4d4bee1ec3938cc7e74d1782656fb14dd
Lun Tan <ltan@ains-inc.com>
2017-08-17 09:49:26 -0400

    in the right pannel

---
+++ app.js
@@ -0,0 +1,135 @@
+window.onload = function() {
+	createAinsBlocks();
+
+	var blocklyArea = document.getElementById('blocklyArea');
+	var blocklyDiv = document.getElementById('blocklyDiv');
+
+	var workspace = Blockly.inject(blocklyDiv,
+	{
+		toolbox: document.getElementById('toolbox'),
+		grid:
+		{spacing: 20,
+			length: 3,
+			colour: '#ccc',
+			snap: true},
+			trashcan: true,
+			horizontalLayout: false,
+			readOnly:false,
+			    //scrollbars:true,
+			    //toolboxPosition:"start",
+			});
+	var onresize = function(e){
+		var element = blocklyArea;
+		var x = 0;
+		var y = 0;
+		do {
+			x += element.offsetLeft;
+			y += element.offsetTop;
+			element = element.offsetParent;
+		} while (element);
+		blocklyDiv.style.left = x + 'px';
+		blocklyDiv.style.top = y + 'px';
+		blocklyDiv.style.width = blocklyArea.offsetWidth + 'px';
+		blocklyDiv.style.top = blocklyArea.offsetHeight + 'px';
+
+	};
+	window.addEventListener('resize', onresize, false);
+	onresize();
+	Blockly.svgResize(workspace);
+
+	workspace.addChangeListener(function(event){
+		if (event.type == Blockly.Events.MOVE) {
+			var xml = Blockly.Xml.workspaceToDom(workspace);
+			var xml_text = Blockly.Xml.domToPrettyText(xml);
+
+			console.log(xml_text);
+		}
+
+	});
+	workspace.addChangeListener(onClicking);
+
+	function onClicking(event) {
+		if (event.type == Blockly.Events.UI && event.element == 'click') {
+					//Using jQuery to create a dialog
+					var blockId = event.blockId;
+					var block = workspace.getBlockById(blockId);
+					var inputList = block.inputList;
+					console.log(block);
+					console.log(inputList);
+
+					if (block.type == 'ains_invoke') {
+						var textData = inputList[0].fieldRow[1].text_;
+						console.log(textData);
+						//workspace.getBlockById(blockId).inputList[0].fieldRow[2].text_ = 'you clicked me, so text is changed';
+					} else if (block.type == 'ains_if') {
+						var ifCondition = inputList[0].fieldRow[1].text_;
+
+						console.log(ifCondition);
+						createDialog(ifCondition);
+
+						// var textInput = 'you clicked me, so condition is changed';
+
+						// //workspace.getBlockById(blockId).getField('condition').setText(textInput);
+						// block.getField('condition').setText(textInput);
+
+
+						 // workspace.getBlockById(blockId).inputList[0].fieldRow[1].text_ = textInput;
+						 // workspace.getBlockById(blockId).inputList[0].fieldRow[1].textElement_.innerHTML = textInput;
+
+						} else if (block.type == 'ains_while') {
+							var whileCondition = inputList[0].fieldRow[1].text_;
+							console.log(whileCondition);
+							createDialog(whileCondition);
+
+						// var textInput = 'you clicked me, so condition is changed';
+						// block.getField('condition').setText(textInput);
+					}
+				}
+
+				function createDialog(typeCondition) {
+					$("#previousCondition").html(typeCondition);
+					var $dialog = $("#dialogCondition").toggle("slide", {direction:"right"});
+
+
+					
+
+				}
+				// function createDialog(typeCondition) {
+
+				// 	$("#previousCondition").html(typeCondition);
+				// 	var $dialog = $("#dialogCondition").dialog({autoOpen: false,});
+
+
+				// 	$dialog.dialog("option","modal","true");
+				// 	$dialog.dialog("option","width",780);
+				// 	$dialog.dialog("option","height",400);
+				// 	$dialog.dialog("option","height","auto");
+				// 	$dialog.dialog("option","show",{effect:"blind",duration:200,});
+				// 	$dialog.dialog("option","hide",{effect:"blind",duration:200,});
+				// 	$dialog.dialog({open:function(event, ui){
+				// 		$('.ui-widget-overlay').bind('click',function(){
+				// 			$dialog.dialog('close');
+				// 		});
+				// 	}});
+
+
+
+				// 	$dialog.dialog({ buttons: [ { text: "Ok", click: function() { $( this ).dialog( "close" ); } } ] });
+
+				// 	$dialog.dialog("open");
+
+
+				// }
+
+			}
+
+			// workspace.addChangeListener(Blockly.Events.Move, function(){
+			// 	var xml = Blockly.Xml.workspaceToDom(workspace);
+			// 	var xml_text = Blockly.Xml.domToPrettyText(xml);
+
+			// 	console.log(xml_text);
+			// });
+
+			
+			
+		}
